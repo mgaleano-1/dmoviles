69ba1de47805c4dc6ffc9368c965abf67062df19
Manuel Galeano <galeanomanue84@gmail.com>
2024-07-31 00:25:03 -0400

    corregido

--- agregarfuncionalidades/lib/main.dart
+++ agregarfuncionalidades/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-
-// ==========================
-// Ejercicio 1: ListView con una Lista Fija
-// ==========================
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 // Clase para representar un Post
 class Post {
@@ -12,32 +10,29 @@ class Post {
   final String body;
 
   Post({required this.id, required this.title, required this.userId, required this.body});
+
+  // Método para crear un Post desde JSON
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      id: json['id'],
+      title: json['title'],
+      userId: json['userId'],
+      body: json['body'],
+    );
+  }
 }
 
-// Crear una lista fija de objetos Post.
-// Crear una lista fija de objetos Post.
-final List<Post> posts = [
-  Post(id: 1, title: '1. sunt aut facere repellat provident occaecati excepturi optio reprehenderit', userId: 1, body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'),
-  Post(id: 2, title: '2. qui est esse', userId: 1, body: 'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla'),
-  Post(id: 3, title: '3. ea molestias quasi exercitationem repellat qui ipsa sit aut', userId: 1, body: 'et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut'),
-  Post(id: 4, title: '4. eum et est occaecati', userId: 1, body: 'ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit'),
-  Post(id: 5, title: '5. nesciunt quas odio', userId: 1, body: 'repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque'),
-  Post(id: 6, title: '6. sunt aut facere repellat provident occaecati excepturi optio reprehenderit', userId: 1, body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'),
-  Post(id: 7, title: '7. qui est esse', userId: 1, body: 'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla'),
-  Post(id: 8, title: '8. ea molestias quasi exercitationem repellat qui ipsa sit aut', userId: 1, body: 'et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut'),
-  Post(id: 9, title: '9. eum et est occaecati', userId: 1, body: 'ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit'),
-  Post(id: 10, title: '10. nesciunt quas odio', userId: 1, body: 'repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque'),
-  Post(id: 11, title: '11. distinctio vitae autem nihil ut molestias quo', userId: 1, body: 'et itaque necessitatibus maxime molestiae qui quas velit\nut aliquid earum ad in dolorem\ndolores omnis soluta totam'),
-  Post(id: 12, title: '12. natus nisi omnis corporis facere molestiae rerum in', userId: 1, body: 'nisi aut doloremque soluta velit\ndolorem facere sint soluta expedita\nreiciendis consectetur velit'),
-  Post(id: 13, title: '13. accusamus eos facilis sint et aut voluptatem', userId: 1, body: 'ab sit a omnis eos\nsoluta neque rerum exercitationem\nveniam occaecati quis'),
-  Post(id: 14, title: '14. quo laboriosam deleniti aut qui', userId: 1, body: 'doloribus est atque dignissimos porro\nnesciunt debitis eos ut\nquia asperiores est et'),
-  Post(id: 15, title: '15. dolores suscipit mollitia', userId: 1, body: 'consequatur pariatur est eos\nqui voluptatem dolores voluptate\nquia voluptate nobis'),
-  Post(id: 16, title: '16. quo deleniti praesentium dicta non quod', userId: 1, body: 'voluptatem quasi sunt laboriosam\ndolorum et occaecati et\nquisquam est illum'),
-  Post(id: 17, title: '17. quasi nulla vero fugit delectus', userId: 1, body: 'eos voluptatibus suscipit est\nad est aliquid rerum\ndolores quasi veritatis'),
-  Post(id: 18, title: '18. sit consequatur vel aliquam', userId: 1, body: 'voluptatem est soluta aliquam\nrepudiandae voluptatum enim\nvoluptatem quibusdam expedita'),
-  Post(id: 19, title: '19. qui exercitationem consectetur', userId: 1, body: 'facilis et nostrum\nnemo magni est mollitia incidunt tenetur dolor'),
-  Post(id: 20, title: '20. omnis iure dignissimos', userId: 1, body: 'odio minus maiores atque in\nquae quidem optio eos nihil eum'),
-];
+// Función para obtener la lista de posts desde una API
+Future<List<Post>> fetchPosts() async {
+  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+
+  if (response.statusCode == 200) {
+    List<dynamic> jsonResponse = json.decode(response.body);
+    return jsonResponse.map((post) => Post.fromJson(post)).toList();
+  } else {
+    throw Exception('Error al cargar los posts');
+  }
+}
 
 // ==========================
 // Ejercicio 2: Acción en los Ítems del ListView
@@ -49,32 +44,44 @@ class PostListScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Posts')),
-      body: ListView.builder(
-        itemCount: posts.length,
-        itemBuilder: (context, index) {
-          return InkWell(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => PostDetailScreen(postId: posts[index].id),
-                ),
-              );
-            },
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.grey[800],
-                borderRadius: BorderRadius.circular(8.0),
-              ),
-              margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-              padding: EdgeInsets.all(16.0),
-              child: Text(
-                posts[index].title,
-                style: TextStyle(color: Colors.white),
-                textAlign: TextAlign.center,
-              ),
-            ),
-          );
+      body: FutureBuilder<List<Post>>(
+        future: fetchPosts(), // Llamada a la función que obtiene los posts
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            final posts = snapshot.data!;
+            return ListView.builder(
+              itemCount: posts.length,
+              itemBuilder: (context, index) {
+                return InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => PostDetailScreen(postId: posts[index].id),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[800],
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                    padding: EdgeInsets.all(16.0),
+                    child: Text(
+                      posts[index].title,
+                      style: TextStyle(color: Colors.white),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                );
+              },
+            );
+          }
         },
       ),
       backgroundColor: Colors.black,
